14f054433c75281f4a63081be08a1f605e47a712
Oliver Seemann <oseemann@jekyll.bln.de.ingenico.com>
2010-01-26 16:06:16 +0100

    move repository scanner code into own subdirectory

--- scan.py
+++ scan.py
@@ -3,17 +3,16 @@
 # vim:set autoindent smarttab nowrap:
 
 import settings
-import repscanner
 
 # import the django environment first before using any local modules
 from django.core.management import setup_environ
 setup_environ(settings)
 from webreview.models import Repository
+from scanner.repscanner import RepositoryScanner
 
 if __name__ == '__main__':
    for r in Repository.objects.all():
       print "Scanning %s" % r.name
-      c = repscanner.RepositoryScanner(r)
-      c.run()
+      RepositoryScanner(r).run()
 
 
